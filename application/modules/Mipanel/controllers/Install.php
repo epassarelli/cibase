99a67ecc68775e13f195c9e3f7531d7501545dca
Eduardo Passarelli <epassarelli@inti.gob.ar>
2019-10-29 17:15:11 -0300

    Inicio de instalacion

--- application/modules/Mipanel/controllers/Install.php
+++ application/modules/Mipanel/controllers/Install.php
@@ -6,22 +6,114 @@ class Install extends MX_Controller {
 
     function __construct() {
         parent::__construct();
+        
+        $this->load->library('migration');
 
-        // if (!$this->ion_auth->logged_in()) {
-        //     redirect('Login');
-        // }
+        if ($this->migration->current() === FALSE)
+        {
+            show_error($this->migration->error_string());
+        } 
+
         $this->load->model('Install_model');
     }
 
 	public function index() {
-        // Insertar los grupos
 
-        // Insertar user Admin + Empresa
+        // Correr las migraciones
+
+        // Insertar los grupos
+        $grupos = array(
+                array(
+                        'id' => 1,
+                        'name' => 'admin',
+                        'description' => 'Super Administrador'
+                ),
+                array(
+                        'id' => 2,
+                        'name' => 'cliente',
+                        'description' => 'Cliente del sitio'
+                )
+        );
+
+        $this->Install_model->insertarGrupos($grupos);
+
+
+
+
+        // Insertar user Admin + SAdmin
+        $usuarios = array(
+                          'id' => 1,  
+                          'ip_address' => '127.0.0.1',  
+                          'username' => 'admin',  
+                          'password' => '$2a$07$SeBknntpZror9uyftVopmu61qg0ms8Qv1yV6FG.kQOSM.9QhmTo36',  
+                          'salt' => '',  
+                          'email' => 'admin@admin.com',  
+                          'activation_code' => '',  
+                          'forgotten_password_code' => NULL,  
+                          'created_on' => '1268889823',  
+                          'last_login' => '1268889823',  
+                          'active' => '1',  
+                          'first_name' => 'Admin',  
+                          'last_name' => 'Admin',  
+                          'company' => 'ADMIN',  
+                          'phone' => '0'
+                            );
+
+        $this->Install_model->insertarUsuarios($usuarios);
 
         // Insertar un registro para empresa
+        $empresa = [
+                        'id' => 1,
+                        'razonsocial' => 'Bondrolfio S.A.',
+                        'direcion'    => '9 de Julio 488',
+                        'cpostal'    => '1653',
+                        'localidad'    => 'San Martin',
+                        'provincia'    => 'Buenos Aires',
+                        'pais'    => 'Argentina',
+                        'cordenadas'    => '9 de Julio 488',
+                        'telefono'    => '4567-8912',
+                        'correo'    => 'bondrolfiosa@bondrolfio.com',
+                        'facebook'    => 'https://www.facebook.com',
+                        'instagram'    => '@bondrolfioSA'
+                ];
 
-        // Insertar un registro para nosotros        
+        $this->Install_model->insertarEmpresa($empresa);
+
+        // Insertar un registro para nosotros
+        $nosotros = [
+                        'username' => 'darth',
+                        'email'    => 'darth@theempire.com'
+                ];
+
+        $this->Install_model->insertarNosotros($nosotros);        
+
+        // Insertar los slides
+        $slider = array(
+                    array(
+                            'title' => 'My title',
+                            'name' => 'My Name',
+                            'date' => 'My date'
+                    ),
+                    array(
+                            'title' => 'Another title',
+                            'name' => 'Another Name',
+                            'date' => 'Another date'
+                    )
+        );
+
+        $this->Install_model->insertarSlides($slider);
+
+        // Insertar los servicios
+        $servicios = [
+                        'username' => 'darth',
+                        'email'    => 'darth@theempire.com'
+                ];
+
+        $this->Install_model->insertarServicios($servicios);
+
 
         $this->template->load('layout_back', 'mipanel_dashboard_view', $this->data);
     }
+
+
 }
--- application/modules/Mipanel/models/Install_model.php
+++ application/modules/Mipanel/models/Install_model.php
@@ -2,15 +2,38 @@
 
 class Install_model extends CI_Model {
 
-    function __construct() {
+    function __construct(){
         parent::__construct();
     }
 
-    public function insertar($value='')
-    {
-    	# code...
+    // Inserta los 2 roles posibles
+    public function insertarGrupos($data){
+		$this->db->insert_batch('groups', $data);	
     }
 
+    // Insertar user Admin + SAdmin
+    public function insertarUsuarios($data){
+		$this->db->insert('users', $data);	
+    }
 
+    // Insertar un registro para empresa
+    public function insertarEmpresa($data){
+		$this->db->insert('empresa', $data);	
+    }
+
+    // Insertar un registro para nosotros
+    public function insertarNosotros($data){
+		$this->db->insert('nosotros', $data);	
+    }       
+
+    // Insertar los slides
+    public function insertarSlides($data){
+		$this->db->insert_batch('slider', $data);	
+    }
+
+    // Insertar los servicios
+    public function insertarServicios($data){
+		$this->db->insert_batch('servicios', $data);	
+    }
 
 }
