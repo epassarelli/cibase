2501e1e1ceb3489e90236142b31ae0fc8c980284
epassarelli <epassarelli@gmail.com>
2021-10-22 11:28:02 -0300

    Se cambió el modelo de home para que traiga el sitio por URL mediante LIKE y las secciones mediante el ID del sitio

--- application/modules/Home/controllers/Home.php
+++ application/modules/Home/controllers/Home.php
@@ -32,31 +32,35 @@ class Home extends MX_Controller {
 	// Carga el Nosotros para el front
 	public function index(){
         
-        $sitio = $this->Home_model->getInfoSitio(base_url());
-        $data['sitio'] = $sitio;
-        $data['secciones'] = $this->Home_model->getSeccionesActivas(base_url());
+        $data['sitio'] = $this->Home_model->getInfoSitio(base_url());
+        
+        // Si encontré un sitio para esa url
+        if($data['sitio']){
+            
+            // Traigo las secciones del sitio
+            $data['secciones'] = $this->Home_model->getSeccionesActivas($data['sitio']->id);
+            $data['landing']    = True;
 
-        // Si no existen datos del sitio en Session los guardo
-        //if( $this->session->userdata('nombre') == null ){
-            foreach ($sitio as $key => $value) {
+            // Guardo todos los datos del sitio en session
+            foreach ($data['sitio'] as $key => $value) {
                 $this->session->set_userdata($key, $value);
             }            
-        //}       
-        // var_dump($sitio);
-        // Si hay secciones creadas y activas las traigo
-        if(count($data['secciones']) > 0){
-            $data['landing']    = True;
-            //$data['secciones']  = $secciones;
-            $data['view']       = 'home_view';
-            $this->load->view('layout_'.$this->session->userdata('theme').'_view', $data);
+
+            // Si hay secciones creadas y activas las traigo
+            if(count($data['secciones']) > 0){
+                $data['view']       = 'home_view';
+                $this->load->view('layout_'.$this->session->userdata('theme').'_view', $data);
+            }
+            else{
+                // Sino muestro sitio en construcción   
+                $this->load->view('sitio_en_construccion.html');         
+            }
         }
+        // Sino muestro sitio en construcción
         else{
             // Sino muestro sitio en construcción   
-            $this->load->view('sitio_en_construccion.html');         
+            $this->load->view('sitio_en_construccion.html');               
         }
-
-        // Sino muestro sitio en construcción
-
 
 	}
 
--- application/modules/Home/models/Home_model.php
+++ application/modules/Home/models/Home_model.php
@@ -1,6 +1,7 @@
 <?php
 
 class Home_model extends CI_Model{
+
   
 public function __construct(){
     parent::__construct();
@@ -10,16 +11,16 @@ public function __construct(){
 public function getInfoSitio($url){
     $this->db->from('sitios si');
     $this->db->join('themes th', 'si.theme_id = th.id');    
-    $this->db->where('si.url', $url);
+    $this->db->like('si.url', $url);
     $query = $this->db->get();
     return $query->row();
 }
 
 // Retorna las secciones activas si las tiene
-public function getSeccionesActivas($url){
+public function getSeccionesActivas($sitio_id){
     $this->db->from('secciones se');
     $this->db->join('sitios si', 'se.sitio_id = si.id');
-    $this->db->where('si.url', $url);
+    $this->db->where('se.sitio_id', $sitio_id);
     $this->db->where('se.estado', 1);
     
     $query = $this->db->get();
