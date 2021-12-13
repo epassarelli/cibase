019cd687bf05f53425d7359789705006589fee85
epassarelli <epassarelli@gmail.com>
2021-12-13 10:04:11 -0300

    Model bloques modificado

--- application/modules/contenidos/models/Bloques_model.php
+++ application/modules/contenidos/models/Bloques_model.php
@@ -15,7 +15,7 @@ class Bloques_model extends CI_Model{
 
     public function getBloquesPor($parametros){
         
-        $this->db->select('b.texto1,b.texto2,b.imagen')
+        $this->db->select('b.bloque_id,b.seccion_id,b.texto1,b.texto2,b.imagen,b.formato_id')
                     ->from('bloques b')
                     ->join('formatos f', 'f.formato_id = b.formato_id')
                     ->where($parametros);
