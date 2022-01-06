427784ef07b7c1838868bc6738516d53a5a7802a
ruben rufino <rrufino71@gmail.com>
2022-01-05 23:33:57 -0300

    abm de productos falta insertar, editar y eliminar

--- application/modules/Mipanel/models/Productos_model.php
+++ application/modules/Mipanel/models/Productos_model.php
@@ -1,6 +1,6 @@
 <?php
 
-class Productos_model extends CI_Model{
+class Productos_model extends MY_Model{
   
 public function __construct() {
     parent::__construct();
@@ -12,6 +12,15 @@ public function setProducto($data)
     $this->db->insert('productos', $data);
 }
 
+
+public function cambioEstado($id,$publicar)
+{
+    $data['publicar'] = ($publicar == 1) ? "0" : "1" ;
+    $this->db->where('id',$id);
+    $this->db->where('sitio_id',$this->config->item('sitio_id'));
+    $this->db->update('productos',$data);
+    return $data['publicar'];
+}
 
 }
 ?>
