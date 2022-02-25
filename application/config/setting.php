2939c53b42a29d88766cdd9cc67e870022e47d38
epassarelli <epassarelli@gmail.com>
2022-02-25 15:52:04 -0300

    Se agrega un nuevo archivo de configuracion para las particularidades nuestras, se llama setting en la carpeta config. Ademas en el mismo se setea el sitio, header y page header por ahora

---
+++ application/config/setting.php
@@ -0,0 +1,43 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Configuraciones varias
+|--------------------------------------------------------------------------
+|
+| Theme, landing, modulos activos
+*/
+
+/*
+|--------------------------------------------------------------------------
+| Sitio seleccionado
+|--------------------------------------------------------------------------
+|
+| 1-cibase, 2-webpass, 3-vitello, 4-claudia, 5-, 6-cabañas
+*/
+
+$config['sitio_id'] 	= 2; 
+
+/*
+|--------------------------------------------------------------------------
+| Sitio seleccionado
+|--------------------------------------------------------------------------
+|
+| 'porto/header_classic'
+| 'porto/header_overview'
+*/
+
+$config['header'] 	= 'porto/header_overview'; 
+
+/*
+|--------------------------------------------------------------------------
+| Sitio seleccionado
+|--------------------------------------------------------------------------
+|
+| 'porto/page_header_overview'
+| 'porto/page_header_colors_primary'
+| 'porto/page_header_classic_small'
+*/
+
+$config['page_header'] 	= 'porto/page_header_classic_small'; 
