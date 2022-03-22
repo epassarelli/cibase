4e50a8d2c760ec5a8b9a83ed71d4e6c68aed192e
Eduardo Passarelli <epassarelli@gmail.com>
2022-03-22 20:09:21 -0300

    Se agrega v.0.0.1 de internacionalizacion del modulo publicaciones

---
+++ application/language/english/publicaciones_lang.php
@@ -0,0 +1,13 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// Fields
+$lang['relacionadas'] 		= 'Related';
+$lang['otrasCategorias'] 	= 'Other categories';
+$lang['compartir'] 			= 'Share on';
+$lang['leerMas'] 			= 'Read more';
+
+$lang['editorial'] 			= 'Editorial';
+$lang['editor'] 			= 'Editor';
+$lang['isbn'] 				= 'ISBN';
+$lang['paginas'] 			= 'Pages';
+$lang['formato'] 			= 'Format';
