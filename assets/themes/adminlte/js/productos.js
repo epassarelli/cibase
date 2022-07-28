662379dc5178ae5700d94a2d76895f515bce4a7a
ruben rufino <rrufino71@gmail.com>
2022-07-28 20:33:51 -0300

    arreglo llave productos en imagenes

--- assets/themes/adminlte/js/productos.js
+++ assets/themes/adminlte/js/productos.js
@@ -307,9 +307,9 @@ function Detalles(body, table) {
             }            
             
             if ( data.publicar == "1" ) {
-              $('td:eq(4)', row).html( "<div class='text-center'><a href='javascript:void(0);' class='publicarI'><i class='fa  fa-toggle-on fa-2x text-green'></i></a></div>" ); 
+              $('td:eq(3)', row).html( "<div class='text-center'><a href='javascript:void(0);' class='publicarI'><i class='fa  fa-toggle-on fa-2x text-green'></i></a></div>" ); 
             }else{
-              $('td:eq(4)', row).html( "<div class='text-center'><a href='javascript:void(0);' class='publicarI'><i class='fa  fa-toggle-off fa-2x text-green'></i></a></div>" ); 
+              $('td:eq(3)', row).html( "<div class='text-center'><a href='javascript:void(0);' class='publicarI'><i class='fa  fa-toggle-off fa-2x text-green'></i></a></div>" ); 
   
             }
           },
