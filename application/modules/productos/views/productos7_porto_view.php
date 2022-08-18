833147c64b4c99f53df54232ea641a46c2137645
ruben rufino <rrufino71@gmail.com>
2022-08-17 22:26:56 -0300

    se cambio el agregar al carrito para que vaya al detalle

--- application/modules/productos/views/productos7_porto_view.php
+++ application/modules/productos/views/productos7_porto_view.php
@@ -24,7 +24,7 @@
 
 							<span class="product-thumb-info border-0">
 								<?php if(parametro(1) == 'S'): ?>
-								<a href="#" onclick="agregarCarro(<?php echo $prod->producto_id ?>,<?php echo $prod->unidadvta ?>)" class="add-to-cart-product bg-color-primary">
+								<a href="<?php echo site_url('productos/detalle/').$prod->producto_id; ?>" class="add-to-cart-product bg-color-primary">
 									<span class="text-uppercase text-1">Agregar al carrito</span>
 								</a>
 								<?php endif; ?>
