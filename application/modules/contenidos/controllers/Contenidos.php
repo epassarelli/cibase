fdd50cface6db9a6e5b06802a659be3374c29307
Eduardo Passarelli <epassarelli@gmail.com>
2022-10-03 13:30:33 -0300

    Cambios en contenidos, buscador y favicon

--- application/modules/contenidos/controllers/Contenidos.php
+++ application/modules/contenidos/controllers/Contenidos.php
@@ -1,118 +1,122 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php defined('BASEPATH') or exit('No direct script access allowed');
 
-class Contenidos extends MX_Controller {
+class Contenidos extends MX_Controller
+{
 
- 	public function __construct() {
-        
-        parent::__construct();
-        
-        switch (ENVIRONMENT){
-            case 'development':
-                $this->output->enable_profiler(TRUE);
-                break;           
-            case 'testing':
-                $this->output->enable_profiler(TRUE);
-                break;
-            case 'production':
-                $this->output->enable_profiler(FALSE);
-                break;
-        }          
-    
-        $this->load->model(array('Secciones_model','Bloques_model','Componentes_model'));
-    }
-	
+	public function __construct()
+	{
+
+		parent::__construct();
+
+		switch (ENVIRONMENT) {
+			case 'development':
+				$this->output->enable_profiler(TRUE);
+				break;
+			case 'testing':
+				$this->output->enable_profiler(TRUE);
+				break;
+			case 'production':
+				$this->output->enable_profiler(FALSE);
+				break;
+		}
+
+		$this->load->model(array('Secciones_model', 'Bloques_model', 'Componentes_model'));
+	}
+
 	// Carga el Nosotros para el front
-	public function index(){
-        $data['title'] = 'Bienvenidas /os';
+	public function index()
+	{
+		$data['title'] = 'Bienvenidas /os';
 
-        switch ($this->session->userdata('site_lang')) {
-            case 'spanish':
-                $pagIncial = 'inicio';
-                break;
-            case 'english':
-                $pagIncial = 'home';
-                break;    
-            default:
-                $pagIncial = 'inicio';
-                break;
-        }
-        
-        
-        $params = array(
-            //'sitio_id' => $this->config->item('sitio_id'),
-            'sitio_id' => $this->session->userdata("idSitio"),
-            'slug'  => $pagIncial, 
-            'estado' => 1
-        );
+		switch ($this->session->userdata('site_lang')) {
+			case 'spanish':
+				$pagIncial = 'inicio';
+				break;
+			case 'english':
+				$pagIncial = 'home';
+				break;
+			default:
+				$pagIncial = 'inicio';
+				break;
+		}
 
-        $secciones = $this->Secciones_model->getSeccionesPor($params);
 
-        if($secciones){
-            
-            $seccion = $secciones[0];
-            $params2 = array(
-                'b.seccion_id' => $seccion->seccion_id,
-                'b.estado' => 1
-            );
+		$params = array(
+			//'sitio_id' => $this->config->item('sitio_id'),
+			'sitio_id' => $this->session->userdata("idSitio"),
+			'slug'  => $pagIncial,
+			'estado' => 1
+		);
 
-            $data['bloques']    = $this->Bloques_model->getBloquesPor($params2);
-            $data['view'] = 'contenidos_view';
-        }
-        else{
-            $data['view'] = '404'.$this->session->userdata('theme').'_view';
-        }
+		$secciones = $this->Secciones_model->getSeccionesPor($params);
 
-        $this->load->view('layout_'.$this->session->userdata('theme').'_view', $data);
+		if ($secciones) {
+
+			$seccion = $secciones[0];
+			$params2 = array(
+				'b.seccion_id' => $seccion->seccion_id,
+				'b.estado' => 1
+			);
+
+			$data['bloques']    = $this->Bloques_model->getBloquesPor($params2);
+			$data['view'] = 'contenidos_view';
+		} else {
+			$data['view'] = '404' . $this->session->userdata('theme') . '_view';
+		}
+
+		$this->load->view('layout_' . $this->session->userdata('theme') . '_view', $data);
 	}
 
 
-    public function pagina($slug=''){
-        
-        $params = array(
-            //'sitio_id' => $this->config->item('sitio_id'),
-            'sitio_id' => $this->session->userdata("idSitio"),
-            'slug'  => $slug, 
-            'estado' => 1
-        );
+	public function pagina($slug = '')
+	{
 
-        $seccion = $this->Secciones_model->getOneBy('secciones', $campos='', $params, $orden='');
-        //$secciones = $this->Secciones_model->getSeccionesPor($params);
+		$params = array(
+			//'sitio_id' => $this->config->item('sitio_id'),
+			'sitio_id' => $this->session->userdata("idSitio"),
+			'slug'  => $slug,
+			'estado' => 1
+		);
 
-        // if($secciones){
-        if($seccion){    
-            $data['title'] = $seccion->titulo;
-            //$seccion = $secciones[0];
-            $params2 = array(
-                'b.seccion_id' => $seccion->seccion_id,
-                'b.estado' => 1
-            );
+		$seccion = $this->Secciones_model->getOneBy('secciones', $campos = '', $params, $orden = '');
+		//$secciones = $this->Secciones_model->getSeccionesPor($params);
 
-            $data['bloques']    = $this->Bloques_model->getBloquesPor($params2);
-            //$data['bloques']    = $this->Bloques_model-getAllBy('bloques', $campos='', $params1, $orden='');
-            $data['view'] = 'contenidos_view';
-        }
-        else{
-            $data['view'] = '404_'.$this->session->userdata('theme').'_view';
-        }
-        
-        $this->load->view('layout_'.$this->session->userdata('theme').'_view', $data);
-    }
-    
+		// if($secciones){
+		if ($seccion) {
+			$data['title'] = $seccion->titulo;
 
-    public function bloque($bloque_id='', $vista=''){
+			if (strlen($seccion->bajada) >= 5) {
+				$data['view'] = $seccion->bajada;
+			} else {
+				$params2 = array(
+					'b.seccion_id' => $seccion->seccion_id,
+					'b.estado' => 1
+				);
 
-        $bloque = $this->Bloques_model->getBloque($bloque_id);
+				$data['bloques']    = $this->Bloques_model->getBloquesPor($params2);
+				$data['view'] = 'contenidos_view';
+			}
+		} else {
+			$data['view'] = '404_' . $this->session->userdata('theme') . '_view';
+		}
 
-        $params2 = array(
-            'bloque_id' => $bloque->bloque_id,
-            'estado' => 1
-        ); 
-        $data['bloque']     = $bloque;
-        // var_dump($data['bloque']);
-        $data['componentes'] = $this->Componentes_model->getComponentesPor($params2);
-        // var_dump($data['componentes']);
-        $this->load->view($vista, $data);
+		$this->load->view('layout_' . $this->session->userdata('theme') . '_view', $data);
+	}
 
-    }
 
+	public function bloque($bloque_id = '', $vista = '')
+	{
+
+		$bloque = $this->Bloques_model->getBloque($bloque_id);
+
+		$params2 = array(
+			'bloque_id' => $bloque->bloque_id,
+			'estado' => 1
+		);
+		$data['bloque']     = $bloque;
+		// var_dump($data['bloque']);
+		$data['componentes'] = $this->Componentes_model->getComponentesPor($params2);
+		// var_dump($data['componentes']);
+		$this->load->view($vista, $data);
+	}
 }
--- application/modules/productos/views/productos_sidebar_porto_view.php
+++ application/modules/productos/views/productos_sidebar_porto_view.php
@@ -1,23 +1,22 @@
 <aside class="sidebar">
-	
-	<!-- 					
-	<form action="page-search-results.html" method="get">
-		<div class="input-group mb-3 pb-1">
-			<input class="form-control text-1" placeholder="Search..." name="s" id="s" type="text">
-			<span class="input-group-append">
-				<button type="submit" class="btn btn-dark text-1 p-2"><i class="fas fa-search m-2"></i></button>
-			</span>
+
+	<h5 class="font-weight-bold pt-3">Buscador</h5>
+	<form action="<?php echo base_url('productos'); ?>" id="formBusqueda" method="post" enctype="multipart/form-data">
+		<div class="input-group">
+			<input type="text" id="articulobuscado" name="articulobuscado" value="<?php echo set_value('articulobuscado', @$articulobuscado); ?>" class="form-control col-sm-10" placeholder="Busqueda de productos" />
+			<button type="submit" class="btn btn-primary"><i class="fas fa-search"></i></button>
 		</div>
-	</form> -->
+	</form>
+
 
 	<h5 class="font-weight-bold pt-3">Categorias</h5>
 	<ul class="nav nav-list flex-column">
 
-      <?php foreach ($categorias as $cat): ?>
+		<?php foreach ($categorias as $cat) : ?>
 
-      	<li class="nav-item"><a class="nav-link" href="<?php echo site_url('productos/categorias/') . $cat->slug; ?>"><?php echo $cat->categoria; ?></a></li>
+			<li class="nav-item"><a class="nav-link" href="<?php echo site_url('productos/categorias/') . $cat->slug; ?>"><?php echo $cat->categoria; ?></a></li>
 
-      <?php endforeach; ?>
+		<?php endforeach; ?>
 
 	</ul>
 
@@ -48,13 +47,13 @@
 				<li>
 					<div class="post-image">
 						<div class="d-block">
-							<a href="<?php echo site_url('productos/detalle/').$prod->producto_id; ?>">
+							<a href="<?php echo site_url('productos/detalle/') . $prod->producto_id; ?>">
 								<img alt="" width="60" height="60" class="img-fluid" src="img/products/product-grey-1.jpg">
 							</a>
 						</div>
 					</div>
 					<div class="post-info">
-						<a href="<?php echo site_url('productos/detalle/').$prod->producto_id; ?>">Photo Camera</a>
+						<a href="<?php echo site_url('productos/detalle/') . $prod->producto_id; ?>">Photo Camera</a>
 						<div class="post-meta text-dark font-weight-semibold">
 							$299
 						</div>
@@ -63,13 +62,13 @@
 				<li>
 					<div class="post-image">
 						<div class="d-block">
-							<a href="<?php echo site_url('productos/detalle/').$prod->producto_id; ?>">
+							<a href="<?php echo site_url('productos/detalle/') . $prod->producto_id; ?>">
 								<img alt="" width="60" height="60" class="img-fluid" src="img/products/product-grey-4.jpg">
 							</a>
 						</div>
 					</div>
 					<div class="post-info">
-						<a href="<?php echo site_url('productos/detalle/').$prod->producto_id; ?>">Luxury bag</a>
+						<a href="<?php echo site_url('productos/detalle/') . $prod->producto_id; ?>">Luxury bag</a>
 						<div class="post-meta text-dark font-weight-semibold">
 							$199
 						</div>
@@ -78,13 +77,13 @@
 				<li>
 					<div class="post-image">
 						<div class="d-block">
-							<a href="<?php echo site_url('productos/detalle/').$prod->producto_id; ?>">
+							<a href="<?php echo site_url('productos/detalle/') . $prod->producto_id; ?>">
 								<img alt="" width="60" height="60" class="img-fluid" src="img/products/product-grey-8.jpg">
 							</a>
 						</div>
 					</div>
 					<div class="post-info">
-						<a href="<?php echo site_url('productos/detalle/').$prod->producto_id; ?>">Military Rucksack</a>
+						<a href="<?php echo site_url('productos/detalle/') . $prod->producto_id; ?>">Military Rucksack</a>
 						<div class="post-meta text-dark font-weight-semibold">
 							$49
 						</div>
