14acf5340f38c77d4c5ae6c30aa849bbd642750f
Eduardo Passarelli <epassarelli@gmail.com>
2022-11-02 11:49:12 -0300

    Se renombran los helpers con minusculas

---
+++ application/helpers/productos_helper.php
@@ -0,0 +1,18 @@
+<?php
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+if (!function_exists('enOferta')) {
+  function enOferta($valor, $desde, $hasta)
+  {
+    $enOferta = false;
+
+    if ($valor > 0) {
+      $hoy = date('Y-m-d', time());
+
+      if (($desde <= $hoy) and ($hoy <= $hasta)) {
+        $enOferta = true;
+      }
+    }
+    return $enOferta;
+  }
+}
